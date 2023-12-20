1310a78bf3131322c92ba415b7524719e6689834
karimovRR <rrkarimov@gmail.com>
2023-12-20 16:33:58 +0500

    mysql

--- mytests/mytests/settings.py
+++ mytests/mytests/settings.py
@@ -92,10 +92,7 @@ DATABASES = {
         'USER': 'RRKarimov',
         'PASSWORD': 'Pp123456',
         'HOST': 'RRKarimov.mysql.pythonanywhere-services.com',
-        'PORT': '3306',
-        'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
-        }
+        'PORT': '3306'
     }
 }
 
