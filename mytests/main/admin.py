011c1338688f2e9c1ba3672a4f73c26024e74998
karimovRR <rrkarimov@gmail.com>
2023-12-22 15:48:06 +0500

    AnswersAdmin настройка

--- mytests/main/admin.py
+++ mytests/main/admin.py
@@ -1,11 +1,12 @@
 
 from django.contrib import admin
 from .models import Tests, Tasks, Answers, Result
+
+
+@admin.register(Answers)
+class AnswersAdmin(admin.ModelAdmin):
+    list_display = ('task', 'desc', 'true')
+
 admin.site.register(Tests)
 admin.site.register(Tasks)
-admin.site.register(Answers)
 admin.site.register(Result)
-
-class AnswersAdmin(admin.ModelAdmin):
-    list_display = ('desc', 'true')
-    ordering = ['task', 'id']
